a548843a27ae50e45c27e4570c4dccbfe9de1517
sylva <sylvesgodswill@gmail.com>
2026-08-24 01:27:05 +0100

    fix: use pointer consumption API from Compose

--- app/src/main/java/com/therealsylva/roaches/ui/screens/PlayerScreen.kt
+++ app/src/main/java/com/therealsylva/roaches/ui/screens/PlayerScreen.kt
@@ -55,7 +55,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.input.pointer.consume
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
