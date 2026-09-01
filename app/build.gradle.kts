4fb5ff32186b956424cf3c09e455f32c9e934e31
sylva <sylvesgodswill@gmail.com>
2026-09-01 15:24:55 +0100

    Add live sports streaming

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.therealsylva.roaches"
         minSdk = 26
         targetSdk = 35
-        versionCode = 19
-        versionName = "0.3.6"
+        versionCode = 20
+        versionName = "0.3.7"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
     }
